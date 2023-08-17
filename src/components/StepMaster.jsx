211d748dc28e2766f2d436839c83a592cf716c82
Andrés Babativa Goyeneche <andres.babativa.goyeneche@gmail.com>
2023-08-17 01:11:29 -0500

    Base Diseño

--- src/components/StepMaster.jsx
+++ src/components/StepMaster.jsx
@@ -1,5 +1,5 @@
 // import React, { useState, useEffect } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import '../scss/app.scss';
 // import { urlFrostings } from '../endpoints';
@@ -9,10 +9,36 @@ import leftColumnBackgroundLogo from '../images/Mis_pastelitos_Bogota_Logo.png';
 import Fondant from '../images/Fondant.png';
 import Crema from '../images/Crema.png';
 import Ganache from '../images/Ganache.png';
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const StepMaster = () => {
-	// const [frostings, setFrostings] = useState([]); // Usar useState para almacenar los datos
+	const [selectedFrosting, setSelectedFrosting] = useState(null);
+	const [formData, setFormData] = useState({
+		step: 1,
+		selectedFrosting: null,
+	});
+
+	const stepTitle = [
+		{
+			title: 'Seleccione Masa',
+			subtitle: 'Seleccione un tipo de masa para la torta.',
+		},
+		{
+			title: 'Seleccione un Relleno',
+			subtitle: 'Seleccione un tipo de relleno para la torta.',
+		},
+		{
+			title: 'Seleccione una Cobertura',
+			subtitle: 'Seleccione un tipo de cobertura para la torta.',
+		},
+		{
+			title: 'Seleccione la Cantidad de Porciones',
+			subtitle: 'Seleccione la cantidad de porciones que tendrá la torta.',
+		},
+	];
+
+	const currentTitle = stepTitle[formData.step - 1];
 
 	const frostingsJson = [
 		{ icon: Crema, frostingName: 'Crema', price: 1000 },
@@ -20,13 +46,37 @@ const StepMaster = () => {
 		{ icon: Fondant, frostingName: 'Fondant', price: 2100 },
 	];
 
+	const handleNextStep = () => {
+		if (formData.step <= 5) {
+			if (formData.step === 1) {
+				const isValid = validateStepOne();
+				if (!isValid) return;
+			}
+
+			console.log(formData);
+			setFormData(prevData => ({ ...prevData, step: prevData.step + 1 }));
+		}
+	};
+
+	const handlePreviousStep = () => {
+		if (formData.step > 1) {
+			setFormData(prevData => ({ ...prevData, step: prevData.step - 1 }));
+		}
+	};
+
+	const validateStepOne = () => {
+		// let isValid = true;
+
+		return true;
+	};
+
 	// useEffect(() => {
 	//	fetchFrostings(); // Llamar a la función fetchData desde useEffect
 	// }, []);
 
 	// const fetchFrostings = async () => {
-    //		try {
- 	//		const response = await axios.get(urlFrostings);
+	//		try {
+	//		const response = await axios.get(urlFrostings);
 	//		console.log(response);
 	//		setFrostings(response.data); // Actualizar el estado con los datos obtenidos
 	//	} catch (error) {
@@ -34,28 +84,54 @@ const StepMaster = () => {
 	//	}
 	// };
 
+	const renderItems = () => {
+		const itemsPerSlide = 2; // Define cuántos elementos por slide deseas mostrar
+		const slides = [];
+
+		for (let i = 0; i < frostingsJson.length; i += itemsPerSlide) {
+			const slideItems = frostingsJson.slice(i, i + itemsPerSlide);
+			const slideContent = slideItems.map((frosting, index) => (
+				<div 	key={index} className='carousel-item'>
+					<div
+						key={index}
+						className={`step-one-box ${
+							selectedFrosting === index ? 'selected' : ''
+						}`}
+						onClick={() => setSelectedFrosting(index)}
+					>
+						<div
+							className='step-two-icon'
+							style={{ backgroundImage: `url(${frosting.icon})` }}
+						/>
+						<div className='step-two-plan-info'>
+							<Typography className='step-two-plan-title'>
+								{frosting.frostingName}{' '}
+							</Typography>
+							<Typography variant='body2' className='step-two-plan-price'>
+								${frosting.price}{' '}
+							</Typography>
+						</div>
+					</div>
+				</div>
+			));
+
+			slides.push(
+				<div key={i} className='carousel-slide'>
+					{slideContent}
+				</div>,
+			);
+		}
+
+		return slides;
+	};
+
 	const renderStepContent = () => {
 		return (
 			<div className='step-two-form-container'>
 				<div className='step-two-plan-container'>
-					{frostingsJson.map((frosting, index) => (
-						<div key={index} className='step-two-plan-box'>
-							{/* Asegúrate de ajustar las propiedades adecuadas para mostrar los datos */}
-							<div
-										className='step-two-icon'
-										style={{ backgroundImage: `url(${frosting.icon})` }}
-									/>
-							<div className='step-two-plan-info'>
-								<Typography className='step-two-plan-title'>
-									{frosting.frostingName} {/* Ajustar esta propiedad según tus datos */}
-								</Typography>
-								<Typography variant='body2' className='step-two-plan-price'>
-									${frosting.price}{' '}
-									{/* Ajustar esta propiedad según tus datos */}
-								</Typography>
-							</div>
-						</div>
-					))}
+					<Carousel showThumbs={false} showIndicators={false}>
+						{renderItems()}
+					</Carousel>
 				</div>
 			</div>
 		);
@@ -78,8 +154,34 @@ const StepMaster = () => {
 					<div className='image-overlay'></div>
 				</div>
 				<div className='right-column'>
-				<h1 className='title-form'>Elige tu Cobertura</h1>
-					{renderStepContent()}</div>
+					<div className='current-step'>
+						<h2 className='step-title'>
+							{currentTitle ? currentTitle.title : ''}
+						</h2>
+						<p className='step-subtitle'>
+							{currentTitle ? currentTitle.subtitle : ''}
+						</p>
+						{renderStepContent()}
+					</div>
+					<div className='step-buttons'>
+						{formData.step > 1 && formData.step < 5 && (
+							<div className='go-back-button' onClick={handlePreviousStep}>
+								Go Back
+							</div>
+						)}
+						{formData.step === 4 ? (
+							<div className='step-button-finished' onClick={handleNextStep}>
+								Confirm
+							</div>
+						) : (
+							formData.step < 4 && (
+								<div className='step-button' onClick={handleNextStep}>
+									Next Step
+								</div>
+							)
+						)}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
